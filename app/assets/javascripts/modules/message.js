6b3951247f39e84fd8415664fc3b2178d1f43a1b
marmasuo <66347205+marmasuo@users.noreply.github.com>
2020-07-16 17:44:42 +0900

    auto update

---
+++ app/assets/javascripts/modules/message.js
@@ -0,0 +1,65 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="message--list" data-message-id=${message.id}>
+          <div class="message--member">
+            <div class="message--writer">
+              ${message.user_name}
+            </div>
+            &nbsp;
+            <div class="message-date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message--detail">
+            ${message.content}
+            <img class="input-box" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+        `<div class="message--list" data-message-id=${message.id}>
+          <div class="message--member">
+            <div class="message--writer">
+              ${message.user_name}
+            </div>
+            &nbsp;
+            <div class="message-date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message--detail">
+            ${message.content}
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
+  $(".new_message").on('submit', function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.message--field').append(html);
+      $('.message--field').animate({ scrollTop: $('.message--field')[0].scrollHeight});
+      $('.new_message')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $('.submit-btn').prop('disabled', false);
+    });
+  });
+});
